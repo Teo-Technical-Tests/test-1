b27190a0da21065cb315ef8e5e4bdb7ba1fde7a7
Teo <teodorolg130@gmail.com>
2022-01-02 18:04:43 +0100

    feat: created interfaces and exposed methods now the components exposes the public methods in order to rerender

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,14 +1,45 @@
 import React, { useState, useEffect, createContext } from "react"
 import Checkout from "../shared/services/Checkout"
+import { ProductInCart, ProductType } from "../shared/types"
 const co = new Checkout()
-export const CheckoutContext = createContext({ cart: { products: co.cart, total: 0 }, co })
+
+interface CheckoutContext {
+	cart: { products: ProductInCart[]; total: number; totalWithoutDiscounts: number }
+	scan: (name: string) => void
+	unscan: (name: string) => void
+	totalItems: number
+}
+
+const contextValue: CheckoutContext = {
+	cart: { products: co.cart, total: 0, totalWithoutDiscounts: 0 },
+	scan: (code: string) => {},
+	unscan: (code: string) => {},
+	totalItems: 0
+}
+
+export const CheckoutContext = createContext(contextValue)
 
 export const CheckoutProvider = (props: any) => {
-	const [cart] = useState({
+	const [cart, setCart] = useState({
 		products: co.cart,
-		total: 0
+		total: 0,
+		totalWithoutDiscounts: 0
 	})
 
-	return <CheckoutContext.Provider value={{ cart, co }}>{props.children}</CheckoutContext.Provider>
+	const scanHandler = (code: string) => {
+		co.scan(code)
+		setCart({ products: co.cart, total: co.total(), totalWithoutDiscounts: co.totalWithoutDiscounts() })
+	}
+
+	const unscanHandler = (code: string) => {
+		co.unscan(code)
+		setCart({ products: co.cart, total: co.total(), totalWithoutDiscounts: co.totalWithoutDiscounts() })
+	}
+
+	return (
+		<CheckoutContext.Provider value={{ cart, scan: scanHandler, unscan: unscanHandler, totalItems: co.totalItems }}>
+			{props.children}
+		</CheckoutContext.Provider>
+	)
 }
 export default CheckoutContext
