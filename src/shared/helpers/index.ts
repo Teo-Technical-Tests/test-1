6653fdcc81885617c4e7bdcd08daa91d2d32bab6
Teo <teodorolg130@gmail.com>
2022-01-02 18:34:29 +0100

    feat: created toFixedFloat utility

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,3 +1,4 @@
 import { imageURL } from "../constants"
 
 export const getImage = (imgname: string): string => `${imageURL}/${imgname}`
+export const toFixedFloat = (value: number, decimals: number): number => parseFloat(value.toFixed(decimals))
