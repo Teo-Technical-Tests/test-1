74c235c02907706edcd5d8851fd7eb55fe559e3a
Teo <teodorolg130@gmail.com>
2022-01-05 10:32:34 +0100

    feat: now wrapper receives an instance of checkout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import ShoppingCart from "./pages/ShoppingCart/ShoppingCart"
+import Checkout from "./shared/services/Checkout"
+import { CheckoutProviderWrapper } from "./context"
 
 function App() {
 	return (
 		<main className='App'>
-			<Routes>
-				<Route path='/' element={<ShoppingCart />} />
-			</Routes>
+			<CheckoutProviderWrapper co={new Checkout()}>
+				<Routes>
+					<Route path='/' element={<ShoppingCart />} />
+				</Routes>
+			</CheckoutProviderWrapper>
 		</main>
 	)
 }
