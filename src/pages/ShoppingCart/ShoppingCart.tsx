f5f376595013fdc5efe942f29c27899e37fe76a0
Teo <teodorolg130@gmail.com>
2022-01-05 10:34:45 +0100

    refactor: moved wrapper up in the tree

--- src/pages/ShoppingCart/ShoppingCart.tsx
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,18 +1,15 @@
 import { useContext } from "react"
 import OrderSummary from "../../components/OrderSummary/OrderSummary"
 import ProductList from "../../components/ProductList/ProductList"
-import products from "../../shared/constants/fakeapi"
-import CheckoutContext, { CheckoutProvider } from "../../context"
+import CheckoutContext from "../../context"
 
 const ShoppingCart = () => {
 	const { cart } = useContext(CheckoutContext)
 
 	return (
 		<>
-			<CheckoutProvider >
-				<ProductList products={cart.products} />
-				<OrderSummary />
-			</CheckoutProvider>
+			<ProductList products={cart.products} />
+			<OrderSummary />
 		</>
 	)
 }
