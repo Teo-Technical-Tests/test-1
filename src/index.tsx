afdddefde474013a880dbb76ab50ccfbaf221306
Teo <teodorolg130@gmail.com>
2022-01-06 23:00:47 +0100

    feat: now index passes down the checkout

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { BrowserRouter } from "react-router-dom"
+import Checkout from "./shared/services/Checkout"
 
+//Comment: We pass the context as a prop so the test can reinstantiate it before each test
+const co = new Checkout()
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<App co={co} />
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
