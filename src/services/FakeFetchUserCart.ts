416aac1d433da7405cc9141bab5110d7eada2857
Teo <teodorolg130@gmail.com>
2022-01-13 19:46:21 +0100

    deploy

--- src/services/FakeFetchUserCart.ts
+++
@@ -1,6 +0,0 @@
-import products from "../shared/constants/fakeapi"
-
-export const fakeFetchUserCart = () =>
-	products.map(product => {
-		return { ...product, discount: product.discount, quantity: 0, subtotal: 0 }
-	})
--- src/shared/helpers/index.ts
+++
@@ -1,4 +0,0 @@
-import { imageURL } from "../constants"
-
-export const getImage = (imgname: string): string => `${imageURL}/${imgname}`
-export const toFixedFloat = (value: number, decimals: number): number => parseFloat(value.toFixed(decimals))
